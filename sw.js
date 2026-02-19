9adba0a93a8a5bd0c544cd4b87eb762d101b3416
wllngtns <well.riviera@gmail.com>
2026-02-19 20:17:12 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open("kanban-cache").then(function (cache) {
+      return cache.addAll(["./"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
